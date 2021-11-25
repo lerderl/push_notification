0202f6587919bed21b33c1cdd315c927ec6835ed
Joseph Olukunle <josepholukunle1107@gmail.com>
2021-11-25 14:52:20 +0100

    Created the subscribe route

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ const privateVapidKey = 'V5kGt30uEhPQx6eJBD06f8dEL4YP87tAPuN28BghxFU';
 
 //setting vapid keys details
 webpush.setVapidDetails('mailto:josepholukunle1107@gmail.com', publicVapidKey,privateVapidKey);
+
+//subscribe route
+app.post('/subscribe', (req, res)=>{
+    //get push subscription object from the request
+    const subscription = req.body;
+
+    //send status 201 for the request
+    res.status(201).json({})
+
+    //create paylod: specified the details of the push notification
+    const payload = JSON.stringify({title: 'My Push Notification' });
+
+    //pass the object into sendNotification function and catch any error
+    webpush.sendNotification(subscription, payload).catch(err=> console.error(err));
+})
