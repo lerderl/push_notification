e1b973288a9c4385e182c5ddb1e06f1f7453cc91
Joseph Olukunle <josepholukunle1107@gmail.com>
2021-11-25 14:41:55 +0100

    Generated and set the VAPID keys

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ const app = express();
 
 //using bodyparser
 app.use(bodyParser.json())
+
+//storing the keys in variables
+const publicVapidKey = 'BLJp1PLhneKlN9YzfnRIotMv3GQZPLNeFoLPUr5pujdG8xw6nLqyJeYpSMncubz2ho2xOfeI1JAA5spVwIIn2Lg';
+const privateVapidKey = 'V5kGt30uEhPQx6eJBD06f8dEL4YP87tAPuN28BghxFU';
+
+//setting vapid keys details
+webpush.setVapidDetails('mailto:josepholukunle1107@gmail.com', publicVapidKey,privateVapidKey);
